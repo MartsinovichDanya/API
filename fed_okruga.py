af412131e608325c235038185129fa8c778c4b8f
Danya <den.martsinovich@yandex.ru>
2019-03-20 22:29:24 +0300

    fed_okruga

---
+++ fed_okruga.py
@@ -0,0 +1,10 @@
+import requests
+
+goroda = ['Хабаровск', 'Уфа', 'Нижний Новгород', 'Калининград']
+
+for gorod in goroda:
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?geocode={gorod}, 1&format=json"
+    result = requests.get(geocoder_request).json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']\
+        ['metaDataProperty']['GeocoderMetaData']['AddressDetails']['Country']['AdministrativeArea']\
+        ['AdministrativeAreaName']
+    print(result)
